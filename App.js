2ac05d0437f2bba0c5a9e338bad59e10604de5b8
Ashutosh Mukherjee <ashutosh.mukherjee@tadigital.com>
2024-08-13 12:28:33 +0530

    igniting our app

--- App.js
+++ App.js
@@ -1,19 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div" , 
-    {id: "parent"} , 
-    [React.createElement("div" , 
-    {id: "child"} , 
-    [React.createElement("h1" , {} , "I am h1 tag") , 
-        React.createElement("h2" , {} , "I am h2 tag")]), 
-        React.createElement("div" , 
-        {id: "child2"} , 
-        [React.createElement("h1" , {} , "I am h1 tag") , 
-            React.createElement("h2" , {} , "I am h2 tag")])]);
+const heading = React.createElement("h1", {id: "heading"}, "Hello World");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
+//React.createElement => ReactElement which is a JS object => HTMLElemeent(render)
+console.log(heading)
 
-console.log(parent) //object
-
-const root = ReactDOM.createRoot(document.getElementById("root"))
-
-root.render(parent)
+// JSX => React.createElement =>ReactElement which is a JS object => HTMLElement(render)
+const jsxHeading = <h1 id="heading">Hello World from jsx</h1>;
+root.render(jsxHeading);
+console.log(jsxHeading)
