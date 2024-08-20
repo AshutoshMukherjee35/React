a0eeddf386dbf983396d435049566031f6c54994
Ashutosh Mukherjee <ashutosh.mukherjee@tadigital.com>
2024-08-21 00:58:33 +0530

    episode-3 part 1 assignment push

--- App.js
+++ App.js
@@ -1,64 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+//react element {TitleComponent}
 
-// JSX => React.createElement =>ReactElement which is a JS object => HTMLElement(render)
-//react element: jsx is a react element
-const jsxHeading = <h1 className="head" tabIndex="1">Hello World from jsx</h1>;
-const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(jsxHeading);
-console.log(jsxHeading)
+const TitleComponent = <h1>This is title component as react element</h1>;
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
-//react components
-//functional component is a function that returns a react element
 
-//This is component composition
-const HeadingComponent = () => {
-    return <h1>Functional Component</h1>
-}
-root.render(<HeadingComponent/>);
-
-const HeadingComponent2 = () => (
-    <div id="header">
-        <HeadingComponent />
-        <h1>Heading component 2</h1>
+const RenderTitleComponent = () => {
+   return (
+    <div>
+        {TitleComponent}
     </div>
-);
-
-const fn = () => true;
-const number = 1000;
-
-const Heading3 = function() {
-    return(
-        <div id="header">
-        {jsxHeading}
-        <HeadingComponent />
-        <HeadingComponent2 />
-        <h1>Heading component 3</h1>
-        {/*u can run any piece of code over here. Any JS code can be run over here. */}
-        <h2> {number} </h2>
-        <h3>{100+200}</h3>
-        {console.log('sssss')}
-    </div>
-    )
+   )
 }
-
-
-//css: cross site scripting
-//if the api gives malicious code. in this case the data variable may pass some malecious code then the attacker can cause 
-//css but JSX sanitizes the data and from that we can avoid the css. So the malecious code won't run as jsx would sanitize it.
-// const data = api.getdata();
-
-const Example = () => {
+const TitleComponent2 = () => {
     return (
-        <div>
-            {/* {data} */}
-            <h1>Example component</h1>
-            {/*2 ways of writing the component */}
-            <HeadingComponent></HeadingComponent>
-            <HeadingComponent/>
-            {Heading3()}
-        </div>
+        <h1>This is title component 2 as react functional component</h1>
     )
-}
-root.render(<Example />)
+};
+
+
+// {<TitleComponent/>} {<TitleComponent></TitleComponent>} 
+const TitleComponentAsFunctionalComponent = () => (
+    <div>
+    <h1>This is title component as functional component</h1>
+    <TitleComponent2></TitleComponent2>
+    </div>
+)
+root.render(<TitleComponentAsFunctionalComponent />);
