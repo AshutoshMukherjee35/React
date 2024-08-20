a9fa62650b149d36e3d1fccfabe0e75e2f01ac65
Ashutosh Mukherjee <ashutosh.mukherjee@tadigital.com>
2024-08-21 00:22:43 +0530

    episode 3 completion

--- App.js
+++ App.js
@@ -1,13 +1,64 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement("h1", {id: "heading"}, "Hello World");
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
-//React.createElement => ReactElement which is a JS object => HTMLElemeent(render)
-console.log(heading)
 
 // JSX => React.createElement =>ReactElement which is a JS object => HTMLElement(render)
-const jsxHeading = <h1 id="heading">Hello World from jsx</h1>;
+//react element: jsx is a react element
+const jsxHeading = <h1 className="head" tabIndex="1">Hello World from jsx</h1>;
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(jsxHeading);
 console.log(jsxHeading)
+
+//react components
+//functional component is a function that returns a react element
+
+//This is component composition
+const HeadingComponent = () => {
+    return <h1>Functional Component</h1>
+}
+root.render(<HeadingComponent/>);
+
+const HeadingComponent2 = () => (
+    <div id="header">
+        <HeadingComponent />
+        <h1>Heading component 2</h1>
+    </div>
+);
+
+const fn = () => true;
+const number = 1000;
+
+const Heading3 = function() {
+    return(
+        <div id="header">
+        {jsxHeading}
+        <HeadingComponent />
+        <HeadingComponent2 />
+        <h1>Heading component 3</h1>
+        {/*u can run any piece of code over here. Any JS code can be run over here. */}
+        <h2> {number} </h2>
+        <h3>{100+200}</h3>
+        {console.log('sssss')}
+    </div>
+    )
+}
+
+
+//css: cross site scripting
+//if the api gives malicious code. in this case the data variable may pass some malecious code then the attacker can cause 
+//css but JSX sanitizes the data and from that we can avoid the css. So the malecious code won't run as jsx would sanitize it.
+// const data = api.getdata();
+
+const Example = () => {
+    return (
+        <div>
+            {/* {data} */}
+            <h1>Example component</h1>
+            {/*2 ways of writing the component */}
+            <HeadingComponent></HeadingComponent>
+            <HeadingComponent/>
+            {Heading3()}
+        </div>
+    )
+}
+root.render(<Example />)
